7b48218d449a5d2cdd21df82eaa57b60074bcdb1
Chris Sonnhalter <cfsonnhalter@gmail.com>
2021-03-26 12:20:18 +0000

    Initial commit

---
+++ GuessingGame.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GuessingGame implements ActionListener{
+  JTextField field;
+  JButton guessButton, resetButton;
+  JLabel labEnter, labGuess, labLast;
+  random r = new random();
+  int number = r;
+
+  GuessingGame(){
+
+  }
+
+
+
+
+}
